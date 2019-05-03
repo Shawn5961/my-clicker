a1d2b92ddacb9788ba7a49a2ef875e7a1e5ed4a4
Shawn5961 <shawn.meas89@gmail.com>
2019-05-03 15:47:03 -0400

    Increase coins variable working

--- src/Components/Clicker.js
+++ src/Components/Clicker.js
@@ -2,28 +2,36 @@ import React from 'react';
 import "../Styles.css"
 
 class Clicker extends React.Component {
-  constructor() {
-    super();
+    constructor(props){
+    super(props);
+    var incrementAmount = 1
+    var incrementSecondsAmount = 0
+
     this.state = {
       coins: 0,
-      coinsPerSecond: 0,
-      coinsToAdd: 1
+      coinsToAdd: incrementAmount,
+      coinsPerSecondToAdd: incrementSecondsAmount
     };
-    
-    this.coinsToAdd = this.coinsToAdd.bind(this);
-    this.addCoinsPerSecond = this.addCoinsPerSecond.bind(this);
+
+    this.getGreeting = this.getGreeting.bind(this);
+    this.incrementCoinsPerSecond = this.incrementCoinsPerSecond.bind(this);
+    this.incrementCoins = this.incrementCoins.bind(this);
     this.addCoin = this.addCoin.bind(this);
   }
-  
-  coinsToAdd() {
+
+  getGreeting(){
+    return "Welcome to Clicker Game"
+  }
+
+  incrementCoins(coinsToIncrement) {
     this.setState(state => ({
-      coinsToAdd: state.coinsToAdd + 1
+      coinsToAdd: state.coinsToAdd + coinsToIncrement
     }));
   }
 
-  addCoinsPerSecond() {
+  incrementCoinsPerSecond(coinsPerSecondToIncrement) {
     this.setState(state => ({
-      coinsPerSecond: state.coinsPerSecond + 1
+      coinsPerSecondToAdd: state.coinsPerSecondToAdd + coinsPerSecondToIncrement
     }));
   }
 
@@ -33,10 +41,16 @@ class Clicker extends React.Component {
     }));
   }
 
+  addCoinsPerSecond() {
+    this.setState(state => ({
+      coins: state.coins + state.coinsToAdd
+    }));
+  }
+
   addCoinInterval() {
     let that = this;
-    for (let i = 0; i < this.state.coinsPerSecond; i++){
-      this.addCoin();
+    for (let i = 0; i < this.state.coinsPerSecondToAdd; i++){
+      this.addCoinsPerSecond();
     }
     setTimeout(() => {that.addCoinInterval(); }, 1000)
   };
@@ -45,30 +59,42 @@ class Clicker extends React.Component {
     this.addCoinInterval()
   };
 
+
+
   render() {
     return (
       <div>
-        You have {this.state.coins} coins.
+        <h1>{this.getGreeting()}</h1>
+        <h1>You have {this.state.coins} coins.</h1>
         <br />
         <button onClick={this.addCoin}>
           Add {this.state.coinsToAdd} coins
         </button>
 
-        <button onClick={this.coinsToAdd}>
-          Get +1 per click
+        <button onClick={() => this.incrementCoins(1)}>
+          Increase coins to add by 1
         </button>
 
-        <button onClick={this.addCoinsPerSecond}>
+        <button onClick={() => this.incrementCoins(10)}>
+          Increase coins to add by 10
+        </button>
+
+        <button onClick={() => this.incrementCoinsPerSecond(1)}>
           Get +1 coin per second
+        </button>  
+
+        <button onClick={() => this.incrementCoinsPerSecond(10)}>
+          Get +10 coins per second
         </button>  
 
         <br />
           Coins to add = {this.state.coinsToAdd}
         <br />
-          Coins per second = {this.state.coinsPerSecond}
+          Coins per second = {this.state.coinsPerSecondToAdd}
       </div>
     );
   }
 }
+
 
 export default Clicker;
